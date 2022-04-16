ff78c967b73aa911199a958b81830c2098efabec
Craig <Craig.samuelson01@outlook.com>
2022-04-16 08:12:13 +0200

    Set defaults in job file

--- plus/run.go
+++ plus/run.go
@@ -2,11 +2,13 @@ package main
 
 import "fmt"
 
-
+const y int = 2
+var z [10]int 
+type foo [] int
 
 func main() {
 	x := []int{1,2,3}
-	fmt.Println(x)
-	y := []int{1,2,3}
-	fmt.Println(y)
+	for ie , edd := range x {
+		fmt.Println(ie , edd)
+	}
 }
