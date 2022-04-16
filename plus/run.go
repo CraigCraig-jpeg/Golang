9e05b93add6996175eab6aa214cab1389dacf9ec
Craig <craig.samuelson@wonga.co.za>
2022-04-16 07:46:00 +0200

    Set defaults in job file

--- plus/run.go
+++ plus/run.go
@@ -2,15 +2,10 @@ package main
 
 import "fmt"
 
-type man [5] int
+
 
 func main() {
-	var foo man
-	foo[0] = 1
-	foo[1] = len(foo)
-	var x [10]int
-	x[0] = 2 
-	fmt.Println(x[0])
-	fmt.Println(foo[1])
+	x := []int{1,2,3}
+	fmt.Println(x)
 
 }
