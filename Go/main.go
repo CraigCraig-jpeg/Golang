5fdbc0286258e72be0fb4c5499c986f19ffbc907
Craig <Craig.samuelson01@outlook.com>
2022-04-16 08:41:04 +0200

    set the yml file

--- Go/main.go
+++ Go/main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt" 
 
+var xy int 
+var xz [10] int 
+type foo int 
+type bar [] int 
+
 func main() {
-	fmt.Println("lol")
+	x := [] int {1 , 2 , 3}
+	fmt.Println(x[2:])
+	for i, v := range x {
+		fmt.Println(i , v)
+	}
 }
