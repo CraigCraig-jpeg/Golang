985d78b9fd589dbe8d01ee7c4f35109ef54e883e
Craig <Craig.samuelson01@outlook.com>
2022-04-17 20:51:26 +0200

    set the yml file

--- Go/struct.go
+++ Go/struct.go
@@ -2,23 +2,36 @@ package main
 
 import "fmt"
 
-type foo struct{
-	 name string
-	 middle string
-	 surname string
+type person struct{
+	name string
+	middle string
+	surname string
+}
+type secretAgent struct{
+	person
+	ltk bool
 }
 
 func main(){
-	i := foo { 
+	i := person { 
 		name : "Craig",
 		middle : "",
 		surname : "Samuelson",
 	}
-	j := foo { 
+	j := person { 
 		name : "James",
 		middle : "jule",
 		surname : "Howard",
 	}
+	k := secretAgent {
+		person : person{
+		name : "Craig",
+		middle : "",
+		surname : "Samuelson",
+		},
+		ltk : true, 
+	}
 	fmt.Println(i.name , i.surname)
 	fmt.Println(j.name , j.middle , j.surname)
+	fmt.Println(k.name , k.surname , k.ltk)
 }
