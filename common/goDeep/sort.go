1bece97d0f6e49d201809c43c65334507493b43c
Craig <Craig.samuelson01@outlook.com>
2022-04-25 08:05:01 +0200

    ls

--- common/goDeep/sort.go
+++ common/goDeep/sort.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
-	
+	xi := []int{4, 7, 3, 42, 99, 18, 16, 56, 12}
+	sort.Ints(xi)
+	xs := []string{"James", "Q", "M", "Moneypenny", "Dr. No"}
+	sort.Strings(xs)
+
+	fmt.Println(xi)
+	fmt.Println(xs)
+
 }
