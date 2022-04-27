3a976bdb788be8cc719441c37b3be9569d1f876e
Craig <Craig.samuelson01@outlook.com>
2022-04-27 21:24:46 +0200

    ls

--- common/goDeep/bcrypt.go
+++ common/goDeep/bcrypt.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
+	password := []byte("MyDarkSecret")
+	passwordEncrypt , err := bcrypt.GenerateFromPassword(password , 4)
+	if err != nil {
+        panic(err)
+    }
+
+	password1 := []byte("MyDarkSecret1")
+	passwordEncrypt1 , err := bcrypt.GenerateFromPassword(password1 , 4)
+	if err != nil {
+        panic(err)
+    }
 	
+	fmt.Println("Comparing ...")
+
+	err = bcrypt.CompareHashAndPassword(passwordEncrypt, password)
+	if err != nil {
+        panic(err)
+    }else{
+		fmt.Println("Success, youre in !")
+	}
 }
