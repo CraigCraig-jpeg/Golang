30164829a54306977fc11d6b92506a7ff96f41a0
Craig <Craig.samuelson01@outlook.com>
2022-04-28 11:45:28 +0200

    ls

---
+++ common/goDeep/waitGroups.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	fmt.Println("OS\t\t" , runtime.GOOS )
+	fmt.Println("ARCH\t\t" , runtime.GOARCH )
+	fmt.Println("CPU\t\t" , runtime.NumCPU() )
+	fmt.Println("Goroutine\t" , runtime.NumGoroutine() )
+	go foo()
+	bar()
+	fmt.Println("CPU\t\t" , runtime.NumCPU() )
+	fmt.Println("Goroutine\t" , runtime.NumGoroutine() )
+}
+
+func foo() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("foo:", i)
+	}
+}
+
+func bar() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("bar:", i)
+	}
+}
