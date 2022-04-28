ce84a534c026c806ac040a7dee3ff4a70b45ded5
Craig <Craig.samuelson01@outlook.com>
2022-04-28 12:02:51 +0200

    ls

--- common/goDeep/waitGroups.go
+++ common/goDeep/waitGroups.go
@@ -3,23 +3,32 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"sync"
 )
 
+var wg sync.WaitGroup
+
 func main() {
+	
+	
 	fmt.Println("OS\t\t" , runtime.GOOS )
 	fmt.Println("ARCH\t\t" , runtime.GOARCH )
 	fmt.Println("CPU\t\t" , runtime.NumCPU() )
 	fmt.Println("Goroutine\t" , runtime.NumGoroutine() )
+
+	wg.Add(1)
 	go foo()
 	bar()
 	fmt.Println("CPU\t\t" , runtime.NumCPU() )
 	fmt.Println("Goroutine\t" , runtime.NumGoroutine() )
+	wg.Wait()
 }
 
 func foo() {
 	for i := 0; i < 10; i++ {
 		fmt.Println("foo:", i)
 	}
+	wg.Done()
 }
 
 func bar() {
