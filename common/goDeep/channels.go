38e77d4c8e1a815aab36033d85d10d07bcdc4f7e
Craig <Craig.samuelson01@outlook.com>
2022-05-04 09:18:21 +0200

    ls

---
+++ common/goDeep/channels.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+// func main() {
+// 	c := make(chan int)
+// 	go func() {
+// 		c <- 1
+// 	}()
+// 	fmt.Println(<-c)
+// }
+
+func main() {
+	c := make(chan int , 2)
+
+	c <- 1 
+	c <- 3
+
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+}
