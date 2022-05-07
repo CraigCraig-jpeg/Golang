4b7d9856c061deb8b7835f5be4044fd9d7f61f8a
Craig <Craig.samuelson01@outlook.com>
2022-05-07 20:20:52 +0200

    hi

---
+++ common/goExercises/leetcode/LinkedListCycle.go
@@ -0,0 +1,18 @@
+//quetsion https://leetcode.com/problems/linked-list-cycle/
+package main 
+
+import (
+	"fmt"
+	"container/list"
+)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func main() {
+	var 
+
+}
