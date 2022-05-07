6bbd3aa0bbc18ab19c0cfe7251584ee9b552850c
Craig <Craig.samuelson01@outlook.com>
2022-05-07 22:03:49 +0200

    hi

--- common/goSyntax/generics.go
+++ common/goSyntax/generics.go
@@ -4,10 +4,17 @@ import (
 	"fmt"
 )
 
+type hotdog int 
+var g hotdog
+
 func main() {
 	fmt.Println(min(1 , 2))
 	fmt.Println(miner[int](1 , 2))
 	fmt.Println(miner2[int](1 , 2))
+
+	g = 20 
+	fmt.Println(miner2(1 , g))
+	fmt.Println(miner3(1 , 7))
 }
 
 //generic variadic parameter with constraints
@@ -18,13 +25,20 @@ func min[T int](x ...T) int {
 	}
 	return i
 }
-
 //generic parameter with no constraints
 func miner[T interface{}](x T , y T) T {
 
 	return x 
 }
-//generic parameter with constraints
-func miner2[T int](x T , y T) T {
+//generic parameter with constraints and support for undelying 
+// types 
+func miner2[T ~int](x T , y T) T {
+	return x + y
+}
+//generic parameter with variadic constraints 
+type variadic interface {
+	float64 | int 
+}
+func miner3[T variadic](x T , y T) T {
 	return x + y
 }
