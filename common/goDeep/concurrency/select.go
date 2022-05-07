acabfdf94bc91a3c41f23948f82f05284e6ea013
Craig <Craig.samuelson01@outlook.com>
2022-05-07 15:23:50 +0200

    ls

--- common/goDeep/concurrency/select.go
+++ common/goDeep/concurrency/select.go
@@ -5,14 +5,23 @@ import (
 )
 
 func main() {
-	eve := make(chan int)
+	even := make(chan int)
 	odd := make(chan int)
-	quit := make(chan int)
+	end := make(chan int)
 
-	//send a message
-	go send()
+	go send(x chan int, y chan int, z chan int)
 }
 
-func send(a chan int, b chan int , c chan int) {
-	
+func send(x, y, z chan<-  int){
+	for i := 0; i < 100; i++ {
+		if i % 2 == 0 {
+			x <- i
+		}
+		if i % 2 == 1 {
+			y <- i
+		}
+		close(x)
+		close(y)
+		q <- 0
+	}
 }
