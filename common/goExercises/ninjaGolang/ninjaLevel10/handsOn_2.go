bad86d41a02a14bbce35910a4a9ad0475869fc3b
Craig <Craig.samuelson01@outlook.com>
2022-05-08 11:00:49 +0200

    hi

---
+++ common/goExercises/ninjaGolang/ninjaLevel10/handsOn_2.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	cs := make(chan int)
+
+	go func() {
+		cs <- 42
+	}()
+	fmt.Println(<-cs)
+
+	fmt.Printf("------\n")
+	fmt.Printf("cs\t%T\n", cs)
+	
+	cr := make(chan int)
+	go func() {
+		cr <- 42
+	}()
+	fmt.Println(<-cr)
+	fmt.Printf("------\n")
+	fmt.Printf("cr\t%T\n", cr)
+}
