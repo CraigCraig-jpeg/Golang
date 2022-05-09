4c480f0218e16a1aca3da2c2775fec4bce22bd46
Craig <Craig.samuelson01@outlook.com>
2022-05-09 10:18:09 +0200

    hi

---
+++ common/goDeep/err/checkErr.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"io"
+)
+
+func main () {
+	f , err := os.Create("names.txt")
+	if err != nil {
+		fmt.Println("Failed to create" , err )
+		return
+	}
+	defer f.Close()
+	r := strings.NewReader("lol")
+
+	io.Copy(r, f)
+}
