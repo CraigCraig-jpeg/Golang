eb11f539745aa3b1433fa7031f6c463e6330cf0e
Craig <Craig.samuelson01@outlook.com>
2022-05-13 10:57:16 +0200

    hi

---
+++ common/goExercises/ninjaGolang/ninjaLevel11/handsOn_4.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+type sqrtError struct {
+	lat  string
+	long string
+	err  error
+}
+
+func (se sqrtError) Error() string {
+	return fmt.Sprintf("math error: %v %v %v", se.lat, se.long, se.err)
+}
+
+func main() {
+	v , err := sqrt(-10.23)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func sqrt(f float64) (float64, error) {
+	x := new(sqrtError)
+	x.Error()
+	if f < 0 {
+		return 0 , x
+	}
+	return 42, nil
+}
